03e3c7b4e022ea91d3af909585dace4be4c01a6e
Saral03 <saralhanda13@gmail.com>
2023-08-18 00:42:38 +0530

    create app component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
-import Note from "./components/Note";
+import App from "./components/App"
+
 
 ReactDOM.render(
-    <div>
-<Header></Header>
-<Note></Note>
-<Footer></Footer>
-</div>
-,document.getElementById("root")
+<App />,document.getElementById("root")
 
 );
