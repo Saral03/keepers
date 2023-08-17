4db4d1a040bab4fc4bafe68db31ebaae8ee0816b
Saral03 <saralhanda13@gmail.com>
2023-08-17 17:02:25 +0530

    create note component

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Note from "./components/Note";
 
 ReactDOM.render(
     <div>
 <Header></Header>
+<Note></Note>
 <Footer></Footer>
 </div>
 ,document.getElementById("root")
