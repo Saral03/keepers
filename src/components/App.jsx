4a03ccab200907d7cb63f3d8819cc19ff1c64511
Saral03 <saralhanda13@gmail.com>
2023-08-18 01:00:35 +0530

    create note.js file and shown hardcoded notes on website using map function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,12 +2,22 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
+function getNotes(noteDetails){
+    return(
+    <Note
+    key={noteDetails.key}
+    title={noteDetails.title}
+    content={noteDetails.content}
+    />
+    );
+}
 function App(){
     return(
         <div>
             <Header></Header>
-            <Note></Note>
+            {notes.map(getNotes)}
             <Footer></Footer>
         </div>
 
